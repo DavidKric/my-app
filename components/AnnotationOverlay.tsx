6b13c2624f90161e882c358b6d74218e0e53ffd1
DavidKric <39348533+DavidKric@users.noreply.github.com>
2025-05-18 01:07:46 +0300

    Refactor annotations to use new provider

--- components/AnnotationOverlay.tsx
+++ components/AnnotationOverlay.tsx
@@ -1,21 +1,21 @@
 'use client'
-import { useContext } from 'react'
 import { BoundingBox } from '@allenai/pdf-components'
-import { AnnotationContext } from '@/context/AnnotationContext'
+import { useAnnotations } from '@/components/context_panel/annotations/AnnotationProvider'
 
 export default function AnnotationOverlay({ pageIndex }: { pageIndex: number }) {
-  const { annotations, setActiveAnnotationId, activeAnnotationId } = useContext(AnnotationContext)
-  const pageAnnots = annotations.filter(a => a.pageNumber === pageIndex)
+  const { state, dispatch } = useAnnotations()
+  const { annotations, selectedAnnotationId } = state
+  const pageAnnots = annotations.filter(a => (a as any).pageNumber === pageIndex)
   return (
     <>
       {pageAnnots.map(a =>
-        a.rects.map((r: any, i: number) => (
+        (a as any).rects.map((r: any, i: number) => (
           <BoundingBox
             key={a.id + i}
             page={pageIndex}
             {...r}
-            className={`bg-yellow-300 bg-opacity-50 ${activeAnnotationId === a.id ? 'ring-2 ring-yellow-400' : ''}`}
-            onClick={() => setActiveAnnotationId(a.id)}
+            className={`bg-yellow-300 bg-opacity-50 ${selectedAnnotationId === a.id ? 'ring-2 ring-yellow-400' : ''}`}
+            onClick={() => dispatch({ type: 'SELECT_ANNOTATION', id: a.id })}
           />
         ))
       )}
